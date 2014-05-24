2e37f29ea76a242d8acf81ce26fbd9520a0b0784
abrarmp <abrarmp@gmail.com>
2014-05-24 18:59:59 -0400

    initial project commit

---
+++ src/com/example/fishfishfish/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.example.fishfishfish;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+
+		
+		
+		// TODO Auto-generated method stub
+	}
+
+}
