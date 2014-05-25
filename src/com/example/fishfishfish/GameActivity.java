83ed73ad75987d8321b32bdc0e6369dce2cbc5ee
abrarmp <abrarmp@gmail.com>
2014-05-24 22:35:48 -0400

    repo fix

--- src/com/example/fishfishfish/GameActivity.java
+++ src/com/example/fishfishfish/GameActivity.java
@@ -11,7 +11,6 @@ public class GameActivity extends Activity {
 	    super.onCreate(savedInstanceState);
 	
 	    // TODO Auto-generated method stub
-	    // Hi this is a change lol
 	}
 
 }
--- src/com/example/fishfishfish/MainActivity.java
+++ src/com/example/fishfishfish/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.fishfishfish;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.media.audiofx.Visualizer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -21,6 +25,11 @@ public class MainActivity extends Activity {
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
 		}
+		
+		//playFreq(20000);
+		Visualizer visualizer = null;
+		visualizer.setEnabled(true);
+	
 	}
 
 	@Override
@@ -41,6 +50,29 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	void playFreq(float frequency)
+	{
+		
+		short[] buffer = new short[1024];
+		@SuppressWarnings("deprecation")
+		AudioTrack track = new AudioTrack(AudioManager.STREAM_MUSIC, 44100, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT, 1024, AudioTrack.MODE_STREAM);
+		float increment = (float)(2*Math.PI) * frequency / 44100; // angular increment for each sample
+		float angle = 0;
+		float samples[] = new float[1024];
+		track.play();
+
+		while (true) 
+		{
+			for (int i = 0; i < samples.length; i++) 
+			{
+			  samples[i] = (float) Math.sin(angle);   //the part that makes this a sine wave....
+			  buffer[i] = (short) (samples[i] * Short.MAX_VALUE);
+			  angle += increment;
+			}
+			track.write( buffer, 0, samples.length );  //write to the audio buffer.... and start all over again!         
+		}
 	}
 
 	/**
