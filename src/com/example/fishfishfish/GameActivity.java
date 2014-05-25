2c083186f95082e3000623e83f10939eb9748c5a
Keith Stoffel <keithstoffel@Keiths-MacBook-Pro.local>
2014-05-25 00:17:26 -0400

    added GPS Class

--- src/com/example/fishfishfish/GameActivity.java
+++ src/com/example/fishfishfish/GameActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class GameActivity extends Activity {
-
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
--- src/com/example/fishfishfish/MainActivity.java
+++ src/com/example/fishfishfish/MainActivity.java
@@ -4,20 +4,62 @@ import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.Toast;
+
+
 
 public class MainActivity extends Activity {
-
+	    
+	Button btnShowLocation;
+	
+    // GPSTracker class
+    GPSTracker gps;
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		btnShowLocation = (Button) findViewById(R.id.GPSButton);
+		
+		btnShowLocation.setOnClickListener(new View.OnClickListener() {
+			
+            @Override
+            public void onClick(View arg0) {        
+                // create class object
+                gps = new GPSTracker(MainActivity.this);
+ 
+                // check if GPS enabled     
+                if(gps.canGetLocation()){
+                     
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+                     
+                    Log.d("activity_main", "latitude: " + latitude + "longitude" + longitude);
+                    
+                    // \n is for new line
+                    //Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();    
+                    //double angle = gps.getAngleTo(latitude, longitude, (double)28.542744, (double)-81.379049);
+                    //Toast.makeText(getApplicationContext(), "Your angle to: " + angle, Toast.LENGTH_LONG).show();
+                    //String dir = gps.getDirectionTo(longitude, (double)-81.379049);
+                    //String dir2 = gps.getDirectionTo2(latitude, (double)28.542744);
+                    //Toast.makeText(getApplicationContext(), "direction: " + dir2 + dir, Toast.LENGTH_LONG).show();
+                }else{
+                    // can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+                    gps.showSettingsAlert();
+                }
+                 
+            }
+        });
+	
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
 		}
